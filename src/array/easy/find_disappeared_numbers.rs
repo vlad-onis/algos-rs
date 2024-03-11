d41e2a8475c26c0bc5c44b4ec52703c127f8f9f9
Vlad Onis <vladonis.biz+opensource@gmail.com>
2024-03-11 10:44:33 +0200

    find disappeared numbers using iterators and combinators

--- src/array/easy/find_disappeared_numbers.rs
+++ src/array/easy/find_disappeared_numbers.rs
@@ -1,13 +1,18 @@
+use std::iter;
+
 pub fn find_disappeared_numbers(mut nums: Vec<i32>) -> Vec<i32> {
-    let mut res = vec![];
+    let mut seq: Vec<i32> = iter::repeat(0).take(nums.len()).collect();
 
-    let n = nums.len();
+    nums.into_iter()
+        .enumerate()
+        .for_each(|(x, y)| seq[(y - 1) as usize] += 1);
 
-    for i in (1..n + 1) {
-        if !nums.contains(&(i as i32)) {
-            res.push(i as i32);
-        }
-    }
+    let res = seq
+        .into_iter()
+        .enumerate()
+        .filter(|(x, y)| *y == 0)
+        .map(|(x, y)| (x + 1) as i32)
+        .collect();
 
     res
 }
@@ -19,6 +24,11 @@ pub mod tests_disappeared_number {
     #[test]
     pub fn test_dis_nr() {
         let nums = vec![4, 3, 2, 7, 8, 2, 3, 1];
-        find_disappeared_numbers(nums);
+        let res = find_disappeared_numbers(nums);
+        assert_eq!(res, vec![5, 6]);
+
+        let nums = vec![1, 1];
+        let res = find_disappeared_numbers(nums);
+        assert_eq!(res, vec![2]);
     }
 }
