3720a33e6292ae68133b879f73263f8de2d2dc51
Vlad Onis <vladonis.biz+opensource@gmail.com>
2024-06-02 15:44:38 +0300

    k radius subarray problem

--- src/array/medium/k_radius_subarray_averages.rs
+++ src/array/medium/k_radius_subarray_averages.rs
@@ -1,5 +1,8 @@
+use core::num;
 /// https://leetcode.com/problems/k-radius-subarray-averages/description/
 use std::iter;
+
+use crate::array::easy::max_average_subarray_1::get_avg;
 
 pub fn get_averages_prefix_sum(nums: Vec<i32>, k: i32) -> Vec<i32> {
     if nums.is_empty() {
@@ -34,28 +37,47 @@ pub fn get_averages_prefix_sum(nums: Vec<i32>, k: i32) -> Vec<i32> {
     results
 }
 
-pub fn get_averages2(nums: Vec<i32>, k: i32) -> Vec<i32> {
+pub fn find_avg(nums: &Vec<i32>, index: usize, k: usize) -> i32 {
+    let mut sum = 0;
+
+    for i in index - k..index + k {
+        sum += nums[i];
+    }
+
+    return sum / (2 * k as i32 + 1);
+}
+
+pub fn get_averages_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    let window_size = (k as usize * 2 + 1) as i32;
+    let mut res: Vec<i32> = iter::repeat(-1).take(nums.len()).collect();
+
     let k = k as usize;
-    let len = 2 * k as u64 + 1;
 
-    let mut result = vec![-1; nums.len()];
-    if nums.len() < len as usize {
-        return result;
+    if k == 0 {
+        return nums;
     }
 
-    let mut sum = nums[..len as usize]
-        .into_iter()
-        .map(|&num| num as u64)
-        .sum::<u64>();
-    result[k] = (sum / len) as i32;
-
-    for i in k + 1..nums.len() - k {
-        sum += nums[i + k] as u64;
-        sum -= nums[i - k - 1] as u64;
-        result[i] = (sum / len) as i32;
+    if 2 * k + 1 > nums.len() {
+        return res;
     }
 
-    result
+    let mut window_sum = 0;
+
+    // Compute the sum of the first window
+    for i in 0..window_size as usize {
+        window_sum += nums[i] as i64;
+    }
+
+    // set the first average
+    res[k] = (window_sum / window_size as i64) as i32;
+
+    for i in window_size as usize..nums.len() {
+        window_sum = window_sum + nums[i] as i64 - nums[i - window_size as usize] as i64;
+        let avg = (window_sum / window_size as i64) as i32;
+        res[i - k] = avg;
+    }
+
+    res
 }
 
 #[cfg(test)]
@@ -75,5 +97,12 @@ pub mod tests_get_averages {
         // 100.000
         let arr: Vec<i32> = iter::repeat(100000).take(100000).collect();
         let res = get_averages_prefix_sum(arr, 4000);
+    }
+
+    #[test]
+    pub fn test_sliding_window() {
+        let arr: Vec<i32> = vec![2, 2, 1];
+        let res = get_averages_sliding_window(arr, 1);
+        println!("{:?}", res);
     }
 }
